3bcbd56058fb1a078fef08545bce92b928e0e2be
Alvin-Tulud <alvintulud@gmail.com>
2025-04-23 20:51:25 -0700

    stuff from lecture animations and sounds

--- Peter'sPinballPurgatory/Assets/Scripts/ControlCenter/gameboard/Bumpers/BumperHit.cs
+++ Peter'sPinballPurgatory/Assets/Scripts/ControlCenter/gameboard/Bumpers/BumperHit.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BumperHit : MonoBehaviour
@@ -5,7 +6,17 @@ public class BumperHit : MonoBehaviour
     float bumperforcemin = 20f;
     float bumperforcemax = 40f;
 
+    
+
     BumperStats bstats;
+
+    AudioSource bsource;
+
+    private void Start()
+    {
+        bsource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -13,6 +24,10 @@ public class BumperHit : MonoBehaviour
             float randbumperforce = UnityEngine.Random.Range(bumperforcemin, bumperforcemax);
             Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
             rb.AddExplosionForce(randbumperforce, transform.position, 360f, 0f, ForceMode.VelocityChange);
+
+            float pitch = Random.Range(-0.8f, 1.2f);
+            bsource.pitch = pitch;
+            bsource.Play();
         }
     }
 }
